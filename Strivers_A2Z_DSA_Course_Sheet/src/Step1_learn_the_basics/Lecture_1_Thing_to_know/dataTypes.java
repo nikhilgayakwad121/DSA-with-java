80dd2c195814deac91c4acf92adfe78784417ae6
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-01 17:59:56 +0530

    xhcvsjhdcvb

--- Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/dataTypes.java
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/dataTypes.java
@@ -1,8 +1,47 @@
 package Step1_learn_the_basics.Lecture_1_Thing_to_know;
 
+import java.util.Scanner;
+
 public class dataTypes {
-    int
+
+    String strivers= "No article ";
+
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter Integer value");
+    int integer = sc.nextInt();
+    System.out.println("integer value "+ integer);
+
+    System.out.println("Enter short value");
+    short shortvalue = sc.nextByte();
+    System.out.println("short value "+ shortvalue);
+
+
+    System.out.println("Enter byte value");
+    byte bytevalue = sc.nextByte();
+    System.out.println("byte value "+ bytevalue);
+
+    System.out.println("Enter long value");
+    long longvalue = sc.nextLong();
+    System.out.println("long value "+ longvalue);
+
+    System.out.println("Enter float value");
+    float floatvalue = sc.nextFloat();
+    System.out.println(" Float value "+ floatvalue);
+
+    System.out.println("Enter boolean value");
+    boolean booleanvalue =sc.nextBoolean();
+    System.out.println("boolean value "+ booleanvalue);
+
+
+    System.out.println("Enter String value");
+    String stringvalue = sc.next();
+    System.out.println("String value "+ stringvalue);
+
+    System.out.println("Enter char value");
+    char charvalue = stringvalue.charAt(0);
+    System.out.println("char value "+ charvalue);
 
     }
 }
---
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/if_Else_statement.java
@@ -0,0 +1,35 @@
+package Step1_learn_the_basics.Lecture_1_Thing_to_know;
+import java.util.Scanner;
+
+public class if_Else_statement {
+
+    String strivers = "https://takeuforward.org/if-else/if-else-statements/";
+
+    public static void main(String[] args) {
+        System.out.println("Enter your  marks");
+      Scanner  sc = new Scanner(System.in);
+      int marks = sc.nextInt();
+
+
+        char grade;
+
+        if (marks < 25) {
+            grade = 'F';
+        } else if (marks <= 44) {
+            grade = 'E';
+        } else if (marks <= 49) {
+            grade = 'D';
+        } else if (marks <= 59) {
+            grade = 'C';
+        } else if (marks <= 69) {
+            grade = 'B';
+        } else if (marks >= 70) {
+            grade = 'A';
+        } else {
+            grade = 'X'; // Use 'X' to indicate invalid marks
+        }
+
+        System.out.println("Grade: " + grade);
+
+    }
+}
