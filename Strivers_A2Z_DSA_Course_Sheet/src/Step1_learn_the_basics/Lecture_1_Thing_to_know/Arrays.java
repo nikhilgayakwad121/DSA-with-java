1cdc9cbcf362a01e1732f1422e0cd5b203b5dd48
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-01 21:34:50 +0530

    grthbgsdayjfdgxsacv

--- Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/Arrays.java
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/Arrays.java
@@ -6,5 +6,26 @@ public class Arrays {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        //Sample array
+        int[] myNum = {10, 20, 30, 40};
+
+        System.out.println("Enter the size of an array");
+        int n = sc.nextInt();
+        //initialization of the array
+        int arr[] = new int[n];
+        System.out.println("Enter the "+n+" element of an array ");
+        //Storing the elements of the array index wise
+        for(int i =0;i< arr.length ;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        // Printing the array
+        for(int i =0;i< arr.length ;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
     }
 }
--- Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/Strings.java
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/Strings.java
@@ -2,8 +2,18 @@ package Step1_learn_the_basics.Lecture_1_Thing_to_know;
 
 public class Strings {
 
-
     public static void main(String[] args) {
+
+        //String literal
+        //To make Java more memory efficient (because no new objects are created if it exists already in the string constant pool).
+        String String_literal = "Geeks for Geeks";
+
+        //Using new keyword
+        //In such a case, JVM will create a new string object in normal (non-pool) heap memory and the literal “Welcome” will be placed in the string constant pool. The variable s will refer to the object in the heap (non-pool)
+        String string_bynewkeyword = new String("Geeks for Geeks");
+
+        System.out.println("String created by the string literal "+ String_literal);
+        System.out.println("String created by the new keyword "+ string_bynewkeyword);
 
     }
 }
