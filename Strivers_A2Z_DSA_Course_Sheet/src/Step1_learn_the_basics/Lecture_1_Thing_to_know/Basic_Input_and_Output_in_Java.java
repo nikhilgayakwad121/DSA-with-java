66c3c148f5d6d7ffa622d9dbc5ec2ed451b6ccda
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-01 17:27:57 +0530

    Added some code

---
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/Basic_Input_and_Output_in_Java.java
@@ -0,0 +1,35 @@
+package Step1_learn_the_basics.Lecture_1_Thing_to_know;
+//import Statement for Taking User Input Using BufferedReader.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import Statement for Taking User Input Using Scanner.
+import java.util.Scanner;
+
+public class Basic_Input_and_Output_in_Java {
+
+    // This Striver sheet url contain theory of basic input output operation in java.
+    String Strivers ="https://takeuforward.org/java/basic-input-and-output-in-java/";
+    static void UsingScanner(){
+        System.out.println("User Entering value by using Scanner");
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+        System.out.println("User Entered value by using Scanner is = "+x);
+    }
+
+    static void UsingBufferedReader() throws IOException {
+        System.out.println("User Entering value by using UsingBufferedReader");
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(is);
+        String input = bf.readLine();
+        int num = Integer.parseInt(input);
+        System.out.println("User Entered value by using the BufferReader is = "+ num);
+    }
+
+    public static void main(String[] args) throws IOException {
+        UsingScanner();
+        UsingBufferedReader();
+    }
+
+
+}
---
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_1_Thing_to_know/dataTypes.java
@@ -0,0 +1,8 @@
+package Step1_learn_the_basics.Lecture_1_Thing_to_know;
+
+public class dataTypes {
+    int
+    public static void main(String[] args) {
+
+    }
+}
