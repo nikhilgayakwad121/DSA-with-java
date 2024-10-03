116c6fc07761a7d6f84609321a1fe370acee7721
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-10-03 12:42:40 +0530

    3 Aug

---
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_4_know_basic_maths/prime_number.java
@@ -0,0 +1,80 @@
+package Step1_learn_the_basics.Lecture_4_know_basic_maths;
+
+class optimal_prime_number {
+
+    public static void main(String[] args) {
+
+        int n = 12;
+        String result = primeNumber(n);
+        System.out.println(result);
+    }
+
+    static String primeNumber(int n) {
+        int count = 0;
+        int Sqrt = (int) (Math.sqrt(n));
+        for (int i = 1; i <= Sqrt; i++) {
+            if (n % i == 0) {
+                count = count + 1;
+                if (i != n / i)
+                    count = count + 1;
+            }
+            if(count> 2)
+                break;
+        }
+        if(count==2)
+            return "prime no";
+        else
+            return "not a  prime number";
+    }
+//    Time Complexity: O(sqrt(N))where N is the input number. The loop iterates up to the square root of n performing constant time operations at each step.
+//
+//    Space Complexity : O(1) as the space complexity remains constant and independent of the input size. Only a fixed amount of memory is required to store the integer variables.
+
+}
+
+
+
+class prime_number {
+    // Function to check if a
+    // given number is prime.
+    static boolean checkPrime(int n) {
+        // Initialize a counter variable to
+        // count the number of factors.
+        int cnt = 0;
+        // Loop through numbers from 1 to n.
+        for (int i = 1; i <= n; i++) {
+            // If n is divisible by i
+            // without any remainder.
+            if (n % i == 0) {
+                // Increment the counter.
+                cnt = cnt + 1;
+            }
+        }
+
+        // If the number of
+        // factors is exactly 2
+        if (cnt == 2) {
+            // Return true, indicating
+            // that the number is prime.
+            return true;
+        }
+        // If the number of
+        // factors is not 2.
+        else {
+            // Return false, indicating
+            // that the number is not prime.
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 1483;
+        boolean isPrime = checkPrime(n);
+        if (isPrime) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
+
