d4aeae4280f1f92b0c72045248d0314de1b568c8
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-10-04 22:05:34 +0530

    Added the java collections

--- Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_3_java_collection/LearnArrayList.java
+++ Strivers_A2Z_DSA_Course_Sheet/src/Step1_learn_the_basics/Lecture_3_java_collection/LearnArrayList.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-class LearnArrayList {
+public class LearnArrayList {
 
     public static void main(String[] args) {
 
