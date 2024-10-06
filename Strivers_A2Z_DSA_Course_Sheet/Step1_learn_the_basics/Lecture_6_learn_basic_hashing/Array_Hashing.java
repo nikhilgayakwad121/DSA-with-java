8f7ebb239e77b0ada65898790971e2bc73b9d6f4
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-10-06 12:48:57 +0530

    Add 6 oct

--- Strivers_A2Z_DSA_Course_Sheet/Step1_learn_the_basics/Lecture_6_learn_basic_hashing/Array_Hashing.java
+++ Strivers_A2Z_DSA_Course_Sheet/Step1_learn_the_basics/Lecture_6_learn_basic_hashing/Array_Hashing.java
@@ -1,4 +1,6 @@
 package Step1_learn_the_basics.Lecture_6_learn_basic_hashing;
 
-public class Hashing_using_hashmap {
+public class Array_Hashing {
+
+
 }
--- Strivers_A2Z_DSA_Course_Sheet/Step1_learn_the_basics/Lecture_6_learn_basic_hashing/Hashing_using_hashmap.java
+++ Strivers_A2Z_DSA_Course_Sheet/Step1_learn_the_basics/Lecture_6_learn_basic_hashing/Hashing_using_hashmap.java
@@ -1,4 +1,6 @@
 package Step1_learn_the_basics.Lecture_6_learn_basic_hashing;
 
-public class Array_Hashing {
+public class Hashing_using_hashmap {
+
+
 }
