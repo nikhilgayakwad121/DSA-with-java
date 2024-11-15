3e1f15f35f311a37e205c9ceaf877b0622878d9d
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-11-15 17:12:32 +0530

    Added the code

---
+++ All topics Java/lectures/19-stacks-n-queues/questions/ValidParenMinAdd.java
@@ -0,0 +1,20 @@
+package questions;
+
+import java.util.Stack;
+class ValidParenMinAdd {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}
