c274b378ea0ab6d89e2fb5326f0ac75f43192b33
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-11-28 21:01:04 +0530

    Added the code

--- 7-Sorting Problem/_1_Selection_Sort.java
+++ 7-Sorting Problem/_1_Selection_Sort.java
@@ -1,2 +1,38 @@
+import java.util.Arrays;
+
 public class _1_Selection_Sort {
+
+    public static void main(String[] args) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+
+        int n = arr.length;
+
+        System.out.println("before selection sort: ");
+
+        System.out.print(Arrays.toString(arr));
+
+        System.out.println();
+
+        selection_Sort(arr, n);
+
+    }
+
+    private static void selection_Sort(int[] arr, int n) {
+
+        for (int i = 0; i < n- 1 ; i++) {
+            int mini = i;
+            for (int j = i +1 ; j < n; j++) {
+                if(arr[j] < arr[mini]){
+                    mini = j;
+                }
+
+            }
+
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] =temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
