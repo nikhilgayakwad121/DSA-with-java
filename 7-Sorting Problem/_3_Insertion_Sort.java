24eeee8ab3a05b015f894ce3ca6c313be2349c0f
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-11-29 10:24:21 +0530

    Added the code

--- 7-Sorting Problem/_3_Insertion_Sort.java
+++ 7-Sorting Problem/_3_Insertion_Sort.java
@@ -17,9 +17,15 @@ public class _3_Insertion_Sort {
     private static void insertion_sort(int[] arr, int n) {
 
         for (int i = 0; i < n; i++) {
-
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j]   =  temp;
+                    j--;
+            }
         }
-
+        System.out.println(Arrays.toString(arr));
     }
 
 }
