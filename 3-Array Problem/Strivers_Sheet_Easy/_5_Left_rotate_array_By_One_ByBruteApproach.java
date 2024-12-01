329e07c5c6d84b813b91b94304e21efcc59da773
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-12-01 16:19:11 +0530

    Added the code

--- 3-Array Problem/Strivers_Sheet_Easy/_5_Left_rotate_array_By_One_ByBruteApproach.java
+++ 3-Array Problem/Strivers_Sheet_Easy/_5_Left_rotate_array_By_One_ByBruteApproach.java
@@ -21,4 +21,33 @@ public class _5_Left_rotate_array_By_One_ByBruteApproach {
         }
 
     }
+//    Time Complexity: O(n), as we iterate through the array only once.
+//    Space Complexity: O(n) as we are using another array of size, same as the given array.
+
+}
+
+
+class _5_Left_rotate_array_By_One_ByOptimalApproach{
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = {1,2,3,4,5};
+        solveOptimal(arr,n);
+    }
+
+    private static void solveOptimal(int[] arr, int n) {
+        int temp = arr[0];  // Storing the first value of variable in the array.
+        for (int i = 0; i < n-1; i++) {
+            arr[i] = arr[i+1];
+        }
+
+        arr[n - 1] = temp; // assigned the value of variable at the last index
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+//    Time Complexity: O(n), as we iterate through the array only once.
+//    Space Complexity: O(1) as no extra space is used
+
 }
