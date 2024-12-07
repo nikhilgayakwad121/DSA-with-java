7d3ff5300e708020c3bdb156f297e98cac88b893
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-12-07 19:37:48 +0530

    7 december

--- 3-Array Problem/Strivers_Sheet_Easy/_6_Rotate_Array_By_D_Places_Using_temp_Array.java
+++ 3-Array Problem/Strivers_Sheet_Easy/_6_Rotate_Array_By_D_Places_Using_temp_Array.java
@@ -1,6 +1,8 @@
 package Strivers_Sheet_Easy;
 
-public class _6_Rotate_Array_By_D_Places_Using_temp_Array {
+import java.util.Collection;
+
+public class _6_Rotate_Array_By_D_Places_Using_temp_Array   {
     public static void main(String[] args) {
 
         int n = 7;
